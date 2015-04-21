8317bb4ada1cbb2152b0b0c5412d97113466f0ea
mikkeldamm <mdgraphics.dk@gmail.com>
2015-04-22 01:02:33 +0200

    Added Processor configuration for Cirqus startup with testable stuff for learning

---
+++ Boligf/Boligf.Application/ProcessorConfiguration.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using d60.Cirqus;
+using d60.Cirqus.Aggregates;
+using d60.Cirqus.Commands;
+using d60.Cirqus.Events;
+using d60.Cirqus.Extensions;
+using d60.Cirqus.MsSql.Config;
+using d60.Cirqus.MsSql.Views;
+using d60.Cirqus.Views.ViewManagers;
+using d60.Cirqus.Views.ViewManagers.Locators;
+
+namespace Boligf.Application
+{
+	public class ProcessorConfiguration
+	{
+		public static ICommandProcessor CommandProcessor { get; private set; }
+
+		public static void Setup()
+		{
+			const string msSqlConnection = @"Data Source=(localdb)\v11.0;Initial Catalog=BoligfTest;Integrated Security=True;Connect Timeout=15;Encrypt=False;TrustServerCertificate=False";
+			const string msSqlEventStoreTableName = "Events";
+
+			MsSqlViewManager<CounterView> viewManager = new MsSqlViewManager<CounterView>(msSqlConnection, "Counters");
+
+			CommandProcessor = d60.Cirqus.CommandProcessor
+				.With()
+				.EventStore(e => e.UseSqlServer(msSqlConnection, msSqlEventStoreTableName))
+				.EventDispatcher(ed => ed.UseViewManagerEventDispatcher(viewManager))
+				.Create();
+
+			var counterView = viewManager.Load("id");
+			var test1 = counterView.CurrentValue;
+
+			CommandProcessor.ProcessCommand(new IncrementCounter("id", 1));
+			//CommandProcessor.ProcessCommand(new IncrementCounter("id", 2));
+			//CommandProcessor.ProcessCommand(new IncrementCounter("id", 3));
+			//CommandProcessor.ProcessCommand(new IncrementCounter("id", 5));
+			//CommandProcessor.ProcessCommand(new IncrementCounter("id", 8));
+		}
+
+		public class IncrementCounter : Command<Counter>
+		{
+			public IncrementCounter(string aggregateRootId, int delta)
+				: base(aggregateRootId)
+			{
+				Delta = delta;
+			}
+
+			public int Delta { get; private set; }
+
+			public override void Execute(Counter aggregateRoot)
+			{
+				aggregateRoot.Increment(Delta);
+			}
+		}
+
+		public class CounterIncremented : DomainEvent<Counter>
+		{
+			public CounterIncremented(int delta)
+			{
+				Delta = delta;
+			}
+
+			public int Delta { get; private set; }
+		}
+
+		public class Counter : AggregateRoot, IEmit<CounterIncremented>
+		{
+			int _currentValue;
+
+			public void Increment(int delta)
+			{
+				Emit(new CounterIncremented(delta));
+			}
+
+			public void Apply(CounterIncremented e)
+			{
+				_currentValue += e.Delta;
+			}
+
+			public int CurrentValue
+			{
+				get { return _currentValue; }
+			}
+
+			public double GetSecretBizValue()
+			{
+				return CurrentValue % 2 == 0
+					? Math.PI
+					: CurrentValue;
+			}
+		}
+
+		public class CounterView : IViewInstance<InstancePerAggregateRootLocator>, ISubscribeTo<CounterIncremented>
+		{
+			public CounterView()
+			{
+				SomeRecentBizValues = new List<double>();
+			}
+
+			public string Id { get; set; }
+
+			public long LastGlobalSequenceNumber { get; set; }
+
+			public int CurrentValue { get; set; }
+
+			public double SecretBizValue { get; set; }
+
+			public List<double> SomeRecentBizValues { get; set; }
+
+			public void Handle(IViewContext context, CounterIncremented domainEvent)
+			{
+				CurrentValue += domainEvent.Delta;
+
+				var counter = context.Load<Counter>(domainEvent.GetAggregateRootId(), domainEvent.GetGlobalSequenceNumber());
+
+				SecretBizValue = counter.GetSecretBizValue();
+
+				SomeRecentBizValues.Add(SecretBizValue);
+
+				// trim to 10 most recent biz values
+				while (SomeRecentBizValues.Count > 10)
+					SomeRecentBizValues.RemoveAt(0);
+			}
+		}
+	}
+}
