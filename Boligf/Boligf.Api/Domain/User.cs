66d3e06beb766f1a72d98064acfe94b281e3bc2b
mikkeldamm <mdgraphics.dk@gmail.com>
2015-04-30 00:27:29 +0200

    Added user aggregate

---
+++ Boligf/Boligf.Api/Domain/User.cs
@@ -0,0 +1,13 @@
+﻿using d60.Cirqus.Aggregates;
+
+namespace Boligf.Api.Domain
+{
+	public class User : AggregateRoot
+	{
+		public string Firstname { get; set; }
+		public string Lastname { get; set; }
+
+		public string Email { get; set; }
+		public string PasswordHash { get; set; }
+	}
+}
