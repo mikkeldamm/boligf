ff31dfe9142727032c797824ad8b9df2e2e009fc
mikkeldamm <mikkeldamm@hotmail.com>
2015-05-16 12:42:40 +0200

    Revert "Added new view for single association and just some small text to test VSCode"
    This reverts commit 7f1f462d87b9d780c843d584452ad5fc9c0a5de7.

--- Boligf/Boligf.Api/Views/Association/GetAllAssociationsView.cs
+++ Boligf/Boligf.Api/Views/Association/GetAllAssociationsView.cs
@@ -26,4 +26,20 @@ namespace Boligf.Api.Views.Association
 			Names.Add(association.Name);
 		}
 	}
+
+	public class GetSingleAssociationsView : IViewInstance<GlobalInstanceLocator>, ISubscribeTo<AssociationCreated>
+	{
+		public string Id { get; set; }
+		public long LastGlobalSequenceNumber { get; set; }
+
+		public string Name { get; set; }
+
+		public void Handle(IViewContext context, AssociationCreated domainEvent)
+		{
+			var aggregateRootId = domainEvent.GetAggregateRootId();
+			var association = context.Load<Domain.Association>(aggregateRootId);
+
+			Name = association.Name;
+		}
+	}
 }
