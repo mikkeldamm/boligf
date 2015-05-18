cbca1ac98a82ce1b1183ce775f7026e8c292a123
mikkeldamm <mikkeldamm@hotmail.com>
2015-05-19 00:38:32 +0200

    Revert 7f1f462..b993f37
    This rolls back to commit 7f1f462d87b9d780c843d584452ad5fc9c0a5de7.

--- Boligf/Boligf.Api/Views/Association/GetSingleAssociationView.cs
+++ Boligf/Boligf.Api/Views/Association/GetSingleAssociationView.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
 using Boligf.Api.Domain.Events;
 using d60.Cirqus.Extensions;
 using d60.Cirqus.Views.ViewManagers;
@@ -6,24 +6,19 @@ using d60.Cirqus.Views.ViewManagers.Locators;
 
 namespace Boligf.Api.Views.Association
 {
-	public class GetAllAssociationsView : IViewInstance<GlobalInstanceLocator>, ISubscribeTo<AssociationCreated>
+	public class GetSingleAssociationsView : IViewInstance<GlobalInstanceLocator>, ISubscribeTo<AssociationCreated>
 	{
 		public string Id { get; set; }
 		public long LastGlobalSequenceNumber { get; set; }
 
-		public List<string> Names { get; set; }
-
-		public GetAllAssociationsView()
-		{
-			Names = new List<string>();
-		}
+		public string Name { get; set; }
 
 		public void Handle(IViewContext context, AssociationCreated domainEvent)
 		{
 			var aggregateRootId = domainEvent.GetAggregateRootId();
 			var association = context.Load<Domain.Association>(aggregateRootId);
 
-			Names.Add(association.Name);
+			Name = association.Name;
 		}
 	}
 }
