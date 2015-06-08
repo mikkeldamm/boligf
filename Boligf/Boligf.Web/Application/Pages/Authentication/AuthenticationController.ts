dab7bfd154836dc87c5f7f60dacef6cac8e57819
mikkeldamm <mdgraphics.dk@gmail.com>
2015-06-08 21:54:40 +0200

    Added ts files that were missing

---
+++ Boligf/Boligf.Web/Application/Pages/Authentication/AuthenticationController.ts
@@ -0,0 +1,53 @@
+﻿module Boligf {
+
+	export interface IAuthenticationControllerScope extends ng.IScope {
+		testM(): void;
+		testM2(): void;
+		lars: string;
+	}
+
+	export class AuthenticationController {
+
+		static $inject = ['$scope', '$http', 'IAuthenticationService', 'IStoreBearerToken'];
+
+		lars: string;
+
+		constructor(
+			private $scope: IAuthenticationControllerScope,
+			private $http: ng.IHttpService,
+			private authenticationService: IAuthenticationService,
+			private bearerTokenStore: Boligf.IStoreBearerToken
+			) {
+
+			console.log("Auth ctrl init");
+			this.lars = "mikkel";
+		}
+
+		testM(): void {
+
+			//var token = this.authenticationService.login("mikkel@damm.dk", "1234");
+
+			console.log("Click event");
+			console.log("-- token: " + this.bearerTokenStore.token);
+
+			this.$http.get('/api/authentication', {}).success((data) => {
+
+				console.log("test call");
+				console.log(data);
+			});
+		}
+
+		testM2(): void {
+
+			console.log("Click event");
+
+			this.$http.get('/api/authentication/4', {}).success((data) => {
+
+				console.log("test call 2");
+				console.log(data);
+			});
+		}
+	}
+
+	Boligf.App.controller("AuthenticationController", Boligf.AuthenticationController);
+}
---
+++ Boligf/Boligf.Web/_references.ts
@@ -0,0 +1,24 @@
+﻿// Typings 
+/// <reference path="Scripts/typings/angularjs/angular.d.ts"/>
+/// <reference path="Scripts/typings/angularjs/angular-route.d.ts"/>
+/// <reference path="Scripts/typings/angularjs/angular-resource.d.ts"/>
+/// <reference path="Scripts/typings/angularjs/angular-cookies.d.ts"/>
+/// <reference path="Scripts/typings/angularjs/angular-mocks.d.ts"/>
+/// <reference path="Scripts/typings/angularjs/angular-sanitize.d.ts"/>
+/// <reference path="Scripts/typings/angularjs/angular-animate.d.ts"/>
+/// <reference path="Scripts/typings/angularjs/angular-translate.d.ts"/>
+/// <reference path="Scripts/typings/angularjs/angular-ui-router.d.ts"/>
+ 
+/// <reference path="Scripts/typings/jquery/jquery.d.ts"/>
+
+// Application statics
+/// <reference path="Application/States.ts"/>
+
+// Application uses
+/// <reference path="Application/BoligfApp.ts"/>
+
+/// <reference path="Application/BearerTokenStorageService.ts"/>
+/// <reference path="Application/BearerTokenInterceptor.ts"/>
+
+/// <reference path="Application/Pages/Authentication/AuthenticationService.ts"/>
+/// <reference path="Application/Pages/Authentication/IsAuthorizedDirective.ts"/>
