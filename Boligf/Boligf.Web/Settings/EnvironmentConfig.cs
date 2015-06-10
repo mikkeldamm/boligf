392c8ef9c7570715bbeff5ea2e2fdc45459caac4
mikkeldamm <mdgraphics.dk@gmail.com>
2015-06-10 18:08:06 +0200

    Added js configuration for web api access

---
+++ Boligf/Boligf.Web/Settings/EnvironmentConfig.cs
@@ -0,0 +1,33 @@
+﻿using System.Configuration;
+using System.Web;
+
+namespace Boligf.Web.Settings
+{
+	public class EnvironmentConfig : IHttpHandler
+	{
+		public bool IsReusable => true;
+
+		public string ApiDomain
+		{
+			get
+			{
+				var apiDomainConfig = ConfigurationManager.AppSettings["apiDomain"];
+                return !string.IsNullOrEmpty(apiDomainConfig) ? apiDomainConfig : "localhost:17776";
+			}
+		}
+
+		public void ProcessRequest(HttpContext context)
+		{
+			context.Response.ContentType = "application/x-javascript";
+			context.Response.Write(GetJavascriptConfiguration());
+		}
+
+		private string GetJavascriptConfiguration()
+		{
+			return string.Format(
+				@"var Boligf = Boligf || (Boligf = {{}});Boligf.Config = {{ ApiProtocol: 'http://', ApiDomainClean: '{0}', ApiDomain: '{0}/api' }};",
+				ApiDomain
+			);
+		}
+	}
+}
