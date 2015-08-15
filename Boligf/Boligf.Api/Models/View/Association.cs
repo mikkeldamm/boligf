222b14fd6c254b3a9550bc04eb38cce5225f5832
mikkeldamm <mdgraphics.dk@gmail.com>
2015-08-15 09:13:14 +0200

    Added missing files from last commit

--- Boligf/Boligf.Api/Models/View/Association.cs
+++ Boligf/Boligf.Api/Models/View/Association.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Text;
 
 namespace Boligf.Api.Models.View
 {
@@ -24,6 +25,36 @@ namespace Boligf.Api.Models.View
 			public string Zip { get; set; }
 			public string City { get; set; }
 			public string Country { get; set; }
+
+			public string GetFullAddress()
+			{
+				var stringBuilder = new StringBuilder();
+
+				stringBuilder.Append(StreetAddress);
+
+				if (!string.IsNullOrWhiteSpace(No))
+				{
+					stringBuilder.Append(" ");
+					stringBuilder.Append(No);
+					stringBuilder.Append(",");
+				}
+
+				if (!string.IsNullOrWhiteSpace(Floor) && !string.IsNullOrWhiteSpace(Door))
+				{
+					stringBuilder.Append(" ");
+					stringBuilder.Append(Floor);
+					stringBuilder.Append(".");
+					stringBuilder.Append(Door);
+					stringBuilder.Append(",");
+				}
+
+				stringBuilder.Append(" ");
+				stringBuilder.Append(Zip);
+				stringBuilder.Append(" ");
+				stringBuilder.Append(City);
+
+				return stringBuilder.ToString();
+			}
 
 			public List<string> Residents { get; set; }
 
